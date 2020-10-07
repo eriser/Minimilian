f612388bd5acd69bf2d61a00a66b08baf2b6607f
Fabrizio DUHEM <fabrizio@eriser.fr>
2020-10-07 21:50:49 +0200

    cleanup

--- include/maximilian/env.h
+++ include/maximilian/env.h
@@ -4,7 +4,7 @@
 
 namespace maximilian {
 
-class Env final : public Processor {
+class Env : public Processor {
 
 public:
   Env(Context &context);
@@ -25,6 +25,24 @@ public:
   long holdtime;
   long holdcount;
   int attackphase, decayphase, sustainphase, holdphase, releasephase;
+};
+
+struct AR final : public Env
+{
+  AR(Context& context) : Env(context) {}
+
+  double process(double input, double attack = 1, double release = 0.9,
+                 long holdtime = 1, int trigger = 0) { return ar(input, attack, release, holdtime, trigger);}
+};
+
+struct ADSR final : public Env {
+  ADSR(Context &context) : Env(context) {}
+
+  double process(double input, double attack = 1, double decay = 0.99,
+                 double sustain = 0.125, double release = 0.9,
+                 long holdtime = 1, int trigger = 0) {
+    return adsr(input, attack, decay, sustain, release, holdtime, trigger);
+  }
 };
 
 } // namespace maximilian
