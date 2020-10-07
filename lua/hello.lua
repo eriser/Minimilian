21aa03da25530a23a689cabf932e180394e3fe29
Fabrizio DUHEM <fabrizio@eriser.fr>
2020-10-07 19:19:06 +0200

    fix lua script

--- lua/hello.lua
+++ lua/hello.lua
@@ -3,7 +3,14 @@ require "lmaximilian"
 local context = lmaximilian.Context()
 local osc1 = lmaximilian.Oscillator(context)
 local osc2 = lmaximilian.Oscillator(context)
+local filter = lmaximilian.Filter(context)
 
 for i = 1, 100 do
-    print( osc1:sawwave(440) + osc2:sawwave(880) )
+    print( 
+        filter:lores(
+            800, 
+            1, 
+            osc1:saw(440) + osc2:saw(880) 
+        )
+    )
 end 
